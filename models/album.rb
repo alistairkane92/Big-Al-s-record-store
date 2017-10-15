a9d05555058e7c49bc412074e36cdcf00b599891
Alistair Kane <vplelmo@hotmail.co.uk>
2017-10-15 11:22:57 +0100

    adds initialize for both artist & album class

--- models/album.rb
+++ models/album.rb
@@ -1,3 +1,11 @@
+require_relative "../db/sql_runner"
+
 class Album
 
-    def initialize(id, name, )
+    def initialize(id, name, artist_id)
+        @id = id
+        @name = name
+        @artist_id = artist_id
+    end
+
+end
---
+++ models/artist.rb
@@ -0,0 +1,13 @@
+require_relative "../db/sql_runner"
+
+class Artist
+
+    def initialize(id, name)
+        @id = options['id']
+        @name = options['name']
+    end
+
+
+
+
+end
